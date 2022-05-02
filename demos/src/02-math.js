59552804ae66bc5b0b793956c0511240abbcd64c
David Rodriguez <62265080+rodriguezespinoza0795@users.noreply.github.com>
2022-05-02 14:40:16 -0500

    init testing project

---
+++ demos/src/02-math.js
@@ -0,0 +1,14 @@
+function sum(a, b) {
+  return a + b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+module.exports = {
+  sum, multiply, divide
+};
---
+++ demos/src/02-math.test.js
@@ -0,0 +1,16 @@
+const { sum , multiply, divide} = require('./02-math');
+
+test('adds 1 + 2 to equal 3', () => {
+  const rta = sum(1, 2)
+  expect(rta).toBe(3);
+});
+
+test('adds 1 * 2 to equal 2', () => {
+  const rta = multiply(1, 2)
+  expect(rta).toBe(2);
+});
+
+test('adds 2 / 2 to equal 1', () => {
+  const rta = divide(2, 2)
+  expect(rta).toBe(1);
+});
