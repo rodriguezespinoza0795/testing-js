91045bb39e153de6ae375912169960b44b1e393e
David Rodriguez <62265080+rodriguezespinoza0795@users.noreply.github.com>
2022-05-26 11:31:07 -0500

    Mocking

--- examples/api/src/services/books.service.test.js
+++ examples/api/src/services/books.service.test.js
@@ -1,9 +1,24 @@
 const BooksService = require('./books.service');
 
+const fakeBooks = [
+  {
+    _id: 1,
+    name: 'Harry Potter',
+  },
+];
+
+const MongoLibStub = {
+  getAll: () => [...fakeBooks],
+  create: () => {},
+};
+
+jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => MongoLibStub));
+
 describe('Test for bookService', () => {
   let service;
   beforeEach(() => {
     service = new BooksService();
+    jest.clearAllMocks();
   });
 
   describe('test for getbooks', () => {
